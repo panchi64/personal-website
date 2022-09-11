b6822eab62e9a1dfc96483d574f561f326bae537
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2022-09-11 17:00:59 -0400

    Fixed sizing and removed redundant code

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,40 +1,47 @@
-import {NextPage} from "next";
-import {useTheme as useNextTheme} from "next-themes";
+import { NextPage } from "next";
+import { useTheme as useNextTheme } from "next-themes";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "styles/404.module.css";
-import {Card, Row, Text} from "@nextui-org/react";
+import styles from "styles/Home.module.css";
+import { Card, Row, Text, Container } from "@nextui-org/react";
 
 const NotFound: NextPage = () => {
-  const {setTheme} = useNextTheme();
+  const { setTheme } = useNextTheme();
   setTheme("dark");
 
   return (
-      <>
-        <Head>
-          <title>Page Not Found</title>
-          <link rel="shortcut icon" href="/favicon.ico"/>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-        </Head>
-        <div className={styles.welcome_card}>
-          <Card isPressable isHoverable variant="bordered" css={{mw: "50vw"}}>
-            <Card.Header>
-              <Row justify="center">
-                <Text h1 css={{
-                  textGradient: "45deg, $yellow600 -20%, $red600 100%",
-                }}>404 - Page Not Found</Text>
-              </Row>
-            </Card.Header>
-            <Card.Divider/>
-            <Card.Body>
-              <Row justify="center">
-                <Image src="https://media.giphy.com/media/Ta3v3I4GI1gH7Rqek6/giphy.gif" width="480" height="264" />
-              </Row>
+    <>
+      <Head>
+        <title>Page Not Found</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <div className={styles.welcome_card_container}>
+        <Container fluid>
+          <Card isPressable isHoverable variant="bordered">
+            <Card.Divider />
+            <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src="https://media.giphy.com/media/Ta3v3I4GI1gH7Rqek6/giphy.gif"
+                objectFit="cover"
+                width="100%"
+                height={264}
+              />
             </Card.Body>
+            <Card.Footer>
+              <Row justify="center">
+                <Text
+                  h3
+                  css={{ textGradient: "45deg, $yellow600 -20%, $red600 100%" }}
+                >
+                  404 - Page Not Found
+                </Text>
+              </Row>
+            </Card.Footer>
           </Card>
-        </div>
-      </>
+        </Container>
+      </div>
+    </>
   );
-}
+};
 
 export default NotFound;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,81 @@
-import {Card, Link, Loading, Row, Spacer, Text} from "@nextui-org/react";
-import type {NextPage} from "next";
-import {useTheme as useNextTheme} from "next-themes";
+import {
+  Card,
+  Link,
+  Loading,
+  Row,
+  Spacer,
+  Text,
+  Container,
+} from "@nextui-org/react";
+import type { NextPage } from "next";
+import { useTheme as useNextTheme } from "next-themes";
 import Head from "next/head";
 import styles from "styles/Home.module.css";
 
 const Home: NextPage = () => {
-    const {setTheme} = useNextTheme();
-    setTheme("dark");
+  const { setTheme } = useNextTheme();
+  setTheme("dark");
 
-    return (
-        <>
-            <Head>
-                <title>Welcome!</title>
-                <link rel="shortcut icon" href="/favicon.ico"/>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            </Head>
-            <div className={styles.welcome_card}>
-                <Card isPressable isHoverable variant="bordered" css={{mw: "50vw"}}>
-                    <Card.Header>
-                        <Row justify="center">
-                            <Text h1 css={{
-                                textGradient: "45deg, $yellow600 -20%, $red600 100%",
-                            }}>Welcome!</Text>
-                        </Row>
-                    </Card.Header>
-                    <Card.Divider/>
-                    <Card.Body>
-                        <Row justify="center">
-                            <Text blockquote lang="en-US">
-                                <h3>The website is under development.</h3>
-                            </Text>
-                        </Row>
-                        <Loading type="points-opacity"/>
-                        <Row justify="center">
-                            <Text blockquote lang="en-US">
-                                Please check back later!
-                            </Text>
-                        </Row>
-                    </Card.Body>
-                    <Card.Footer>
-                        <Row justify="center">
-                            <Link
-                                color="primary"
-                                target="_blank"
-                                href="https://github.com/panchi64"
-                            >
-                                GitHub Profile
-                            </Link>
-                            <Spacer x={2}/>
-                            <Link
-
-                                color="primary"
-                                target="_blank"
-                                href="https://www.linkedin.com/in/francisco-a-casiano-rosado/"
-                            >
-                                LinkedIn Profile
-                            </Link>
-                        </Row>
-                    </Card.Footer>
-                </Card>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Welcome!</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className={styles.welcome_card_container}>
+        <Container fluid>
+          <Card isPressable isHoverable variant="bordered">
+            <Card.Header>
+              <Row justify="center">
+                <Text
+                  h1
+                  css={{
+                    textGradient: "45deg, $yellow600 -20%, $red600 100%",
+                  }}
+                >
+                  Welcome!
+                </Text>
+              </Row>
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+              <Row justify="center">
+                <Text blockquote lang="en-US">
+                  <h3>The website is under development.</h3>
+                </Text>
+              </Row>
+              <Loading type="points-opacity" />
+              <Row justify="center">
+                <Text blockquote lang="en-US">
+                  Please check back later!
+                </Text>
+              </Row>
+            </Card.Body>
+            <Card.Footer>
+              <Row justify="center">
+                <Link
+                  color="primary"
+                  target="_blank"
+                  href="https://github.com/panchi64"
+                >
+                  GitHub
+                </Link>
+                <Spacer x={2} />
+                <Link
+                  color="primary"
+                  target="_blank"
+                  href="https://www.linkedin.com/in/francisco-a-casiano-rosado/"
+                >
+                  LinkedIn
+                </Link>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default Home;
