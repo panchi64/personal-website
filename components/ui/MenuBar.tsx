9f7331d0e417e4e9801064628df110ae24064a83
Francisco Casiano <personal@franciscocasiano.com>
2023-01-20 21:09:59 -0400

    Limited max width of the Menu Bar

--- components/ui/MenuBar.tsx
+++ components/ui/MenuBar.tsx
@@ -6,7 +6,7 @@ type MenuProps = {
 const MenuBar = (props: MenuProps) => {
   return (
     <>
-      <div className="bg-light-primary/[.72] dark:bg-dark-primary/[.72] border-r-light-secondary dark:border-r-dark-black border-r-[1px] backdrop-blur-md md:w-1/4 lg:w-1/6 max-w-120 h-screen grid-cols-1 p-4 z-50">
+      <div className="bg-light-primary/[.72] dark:bg-dark-primary/[.72] border-r-light-secondary dark:border-r-dark-black border-r-[1px] backdrop-blur-md md:w-1/4 lg:w-1/6 max-w-sm h-screen grid-cols-1 p-4 z-50">
         {props.children}
       </div>
     </>
