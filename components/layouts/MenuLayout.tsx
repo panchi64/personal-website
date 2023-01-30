ba8d63d4ddfcfee11667efac477e68773327f1d3
Francisco Casiano <personal@franciscocasiano.com>
2023-01-29 20:21:51 -0400

    Fixed the previous menubar spacing solution
    - The previous menubar spacing was basically hardcoded so that the menu looks as expected when the sections overflowed. This new solution maintains the expected visual of the menu even when the sections are not full and overflowing.

--- components/layouts/MenuLayout.tsx
+++ components/layouts/MenuLayout.tsx
@@ -1,4 +1,3 @@
-import { Children } from 'react';
 import MenuBar from '../ui/menu/MenuBar';
 import MenuFooter from '../ui/menu/MenuFooter';
 import MenuSections from '../ui/menu/MenuSections';
@@ -7,256 +6,120 @@ import SearchBar from '../ui/SearchBar';
 
 // TODO: Delete the extra copies once the overflow-y-auto is fixed
 const menuSectionPayloadExample: any = {
-  sections: [
-    {
-      title: 'professional',
-      content: [
+    sections: [
         {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
+            title: 'professional',
+            content: [
+                {
+                    sectionName: 'work',
+                    sectionIcon: 'bi-briefcase',
+                },
+                {
+                    sectionName: 'projects',
+                    sectionIcon: 'bi-kanban',
+                },
+                {
+                    sectionName: 'research',
+                    sectionIcon: 'bi-laptop',
+                },
+            ],
         },
         {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
+            title: 'hobbies',
+            content: [
+                {
+                    sectionName: 'blog',
+                    sectionIcon: 'bi-pencil-square',
+                },
+                {
+                    sectionName: 'photography',
+                    sectionIcon: 'bi-camera2',
+                },
+                {
+                    sectionName: 'sports',
+                    sectionIcon: 'bi-bicycle',
+                },
+            ],
         },
         {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
+            title: 'tools',
+            content: [
+                {
+                    sectionName: 'finance',
+                    sectionIcon: 'bi-cash-stack',
+                },
+                {
+                    sectionName: 'productivity',
+                    sectionIcon: 'bi-diagram-3',
+                },
+                {
+                    sectionName: 'oogie boogie',
+                    sectionIcon: 'bi-diagram-3',
+                },
+            ],
         },
-      ],
-    },
-    {
-      title: 'hobbies',
-      content: [
-        {
-          sectionName: 'blog',
-          sectionIcon: 'bi-pencil-square',
-        },
-        {
-          sectionName: 'photography',
-          sectionIcon: 'bi-camera2',
-        },
-        {
-          sectionName: 'sports',
-          sectionIcon: 'bi-bicycle',
-        },
-      ],
-    },
-    {
-      title: 'tools',
-      content: [
-        {
-          sectionName: 'finance',
-          sectionIcon: 'bi-cash-stack',
-        },
-        {
-          sectionName: 'productivity',
-          sectionIcon: 'bi-diagram-3',
-        },
-        {
-          sectionName: 'oogie boogie',
-          sectionIcon: 'bi-diagram-3',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-    {
-      title: 'professional',
-      content: [
-        {
-          sectionName: 'work',
-          sectionIcon: 'bi-briefcase',
-        },
-        {
-          sectionName: 'projects',
-          sectionIcon: 'bi-kanban',
-        },
-        {
-          sectionName: 'research',
-          sectionIcon: 'bi-laptop',
-        },
-      ],
-    },
-  ],
+    ],
 };
 
 const socialDetailsPayloadExample: any = [
-  {
-    url: 'https://www.linkedin.com/in/francisco-a-casiano-rosado/',
-    name: 'linkedin',
-    icon: 'bi-linkedin',
-  },
-  {
-    url: 'https://github.com/panchi64',
-    name: 'github',
-    icon: 'bi-github',
-  },
-  {
-    url: 'mailto:contact@franciscocasiano.com',
-    name: 'email',
-    icon: 'bi-envelope-at-fill',
-  },
+    {
+        url: 'https://www.linkedin.com/in/francisco-a-casiano-rosado/',
+        name: 'linkedin',
+        icon: 'bi-linkedin',
+    },
+    {
+        url: 'https://github.com/panchi64',
+        name: 'github',
+        icon: 'bi-github',
+    },
+    {
+        url: 'mailto:contact@franciscocasiano.com',
+        name: 'email',
+        icon: 'bi-envelope-at-fill',
+    },
 ];
 
 const MenuLayout = (props: { children: any }) => {
-  const { children } = props;
+    const {children} = props;
 
-  return (
-    <>
-      <MenuBar>
-        <div className='grid grid-col justify-center h-full'>
-          <div className='max-w-[240px] overflow-hidden grid grid-flow-row'>
-            <SearchBar />
-            <ProfileSection
-              title='francisco casiano'
-              subtitle='about'
-            />
-            {/* TODO: Find a way to style the scrollbar so it doesn't look so jarring? */}
-            <div className='overflow-y-auto'>
-              {menuSectionPayloadExample.sections.map((section: any) => {
-                return (
-                  <MenuSections
-                    payload={section}
-                    key={section.title}
-                  />
-                );
-              })}
+    return (
+        <>
+            <MenuBar>
+                <div className='grid grid-col justify-center h-full'>
+                    <div className='max-w-[240px] overflow-hidden flex flex-col h-full'>
+                        <SearchBar/>
+
+                        <ProfileSection
+                            title='francisco casiano'
+                            subtitle='about'
+                        />
+                        {/* TODO: Find a way to style the scrollbar so it doesn't look so jarring? */}
+                        <div className='overflow-y-auto flex-grow'>
+                            {menuSectionPayloadExample.sections.map((section: any) => {
+                                return (
+                                    <MenuSections
+                                        payload={section}
+                                        key={section.title}
+                                    />
+                                );
+                            })}
+                        </div>
+                        <div className='w-full my-2'>
+                            <MenuFooter socialDetails={socialDetailsPayloadExample}/>
+                        </div>
+                    </div>
+                </div>
+            </MenuBar>
+            <div className='h-screen flex flex-row bg-light-background dark:bg-dark-background'>
+                {/* Spacer to prevent page content get stuck behind the  */}
+                <div className='sm:w-1/3 lg:w-1/5 sm:max-w-xs'></div>
+                <div className='w-full h-full'>
+                    poopy
+                    {/* children */}
+                </div>
             </div>
-            <div className='w-full my-2'>
-              <MenuFooter socialDetails={socialDetailsPayloadExample} />
-            </div>
-          </div>
-        </div>
-      </MenuBar>
-      {/* TODO: Add spacing so that the content on the page does not cross over to behind the menubar */}
-      <div className='h-screen flex flex-row bg-light-background dark:bg-dark-background'>
-        {/* Spacer to prevent page content get stuck behind the  */}
-        <div className='sm:w-1/3 lg:w-1/5 sm:max-w-xs'></div>
-        <div className='w-full h-full'>
-          poopy
-          {/* children */}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default MenuLayout;
