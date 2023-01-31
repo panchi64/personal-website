b359f769904b5d9cf3d63fa99a32ca3b52ebc66c
Francisco Casiano <34542704+panchi64@users.noreply.github.com>
2023-01-31 09:49:05 -0400

    Updated tools section

--- components/layouts/MenuLayout.tsx
+++ components/layouts/MenuLayout.tsx
@@ -6,120 +6,120 @@ import SearchBar from '../ui/SearchBar';
 
 // TODO: Delete the extra copies once the overflow-y-auto is fixed
 const menuSectionPayloadExample: any = {
-    sections: [
+  sections: [
+    {
+      title: 'professional',
+      content: [
         {
-            title: 'professional',
-            content: [
-                {
-                    sectionName: 'work',
-                    sectionIcon: 'bi-briefcase',
-                },
-                {
-                    sectionName: 'projects',
-                    sectionIcon: 'bi-kanban',
-                },
-                {
-                    sectionName: 'research',
-                    sectionIcon: 'bi-laptop',
-                },
-            ],
+          sectionName: 'work',
+          sectionIcon: 'bi-briefcase',
         },
         {
-            title: 'hobbies',
-            content: [
-                {
-                    sectionName: 'blog',
-                    sectionIcon: 'bi-pencil-square',
-                },
-                {
-                    sectionName: 'photography',
-                    sectionIcon: 'bi-camera2',
-                },
-                {
-                    sectionName: 'sports',
-                    sectionIcon: 'bi-bicycle',
-                },
-            ],
+          sectionName: 'projects',
+          sectionIcon: 'bi-kanban',
         },
         {
-            title: 'tools',
-            content: [
-                {
-                    sectionName: 'finance',
-                    sectionIcon: 'bi-cash-stack',
-                },
-                {
-                    sectionName: 'productivity',
-                    sectionIcon: 'bi-diagram-3',
-                },
-                {
-                    sectionName: 'oogie boogie',
-                    sectionIcon: 'bi-diagram-3',
-                },
-            ],
+          sectionName: 'research',
+          sectionIcon: 'bi-laptop',
         },
-    ],
+      ],
+    },
+    {
+      title: 'hobbies',
+      content: [
+        {
+          sectionName: 'blog',
+          sectionIcon: 'bi-pencil-square',
+        },
+        {
+          sectionName: 'photography',
+          sectionIcon: 'bi-camera2',
+        },
+        {
+          sectionName: 'sports',
+          sectionIcon: 'bi-bicycle',
+        },
+      ],
+    },
+    {
+      title: 'tools',
+      content: [
+        {
+          sectionName: 'finance',
+          sectionIcon: 'bi-cash-stack',
+        },
+        {
+          sectionName: 'productivity',
+          sectionIcon: 'bi-diagram-3',
+        },
+        {
+          sectionName: 'Automation',
+          sectionIcon: 'bi-code-square',
+        },
+      ],
+    },
+  ],
 };
 
 const socialDetailsPayloadExample: any = [
-    {
-        url: 'https://www.linkedin.com/in/francisco-a-casiano-rosado/',
-        name: 'linkedin',
-        icon: 'bi-linkedin',
-    },
-    {
-        url: 'https://github.com/panchi64',
-        name: 'github',
-        icon: 'bi-github',
-    },
-    {
-        url: 'mailto:contact@franciscocasiano.com',
-        name: 'email',
-        icon: 'bi-envelope-at-fill',
-    },
+  {
+    url: 'https://www.linkedin.com/in/francisco-a-casiano-rosado/',
+    name: 'linkedin',
+    icon: 'bi-linkedin',
+  },
+  {
+    url: 'https://github.com/panchi64',
+    name: 'github',
+    icon: 'bi-github',
+  },
+  {
+    url: 'mailto:contact@franciscocasiano.com',
+    name: 'email',
+    icon: 'bi-envelope-at-fill',
+  },
 ];
 
 const MenuLayout = (props: { children: any }) => {
-    const {children} = props;
+  const { children } = props;
 
-    return (
-        <>
-            <MenuBar>
-                <div className='grid grid-col justify-center h-full'>
-                    <div className='max-w-[240px] overflow-hidden flex flex-col h-full'>
-                        <SearchBar/>
+  return (
+    <>
+      <MenuBar>
+        <div className='grid grid-col justify-center h-full'>
+          <div className='max-w-[240px] overflow-hidden flex flex-col h-full'>
+            <SearchBar />
 
-                        <ProfileSection
-                            title='francisco casiano'
-                            subtitle='about'
-                        />
-                        {/* TODO: Find a way to style the scrollbar so it doesn't look so jarring? */}
-                        <div className='overflow-y-auto flex-grow'>
-                            {menuSectionPayloadExample.sections.map((section: any) => {
-                                return (
-                                    <MenuSections
-                                        payload={section}
-                                        key={section.title}
-                                    />
-                                );
-                            })}
-                        </div>
-                        <div className='w-full my-2'>
-                            <MenuFooter socialDetails={socialDetailsPayloadExample}/>
-                        </div>
-                    </div>
-                </div>
-            </MenuBar>
-            <div className='h-screen flex flex-row bg-light-background dark:bg-dark-background'>
-                {/* Spacer to prevent page content get stuck behind the  */}
-                <div className='sm:w-1/3 lg:w-1/5 sm:max-w-xs'></div>
-                <div className='w-full h-full'>
-                    poopy
-                    {/* children */}
-                </div>
+            <ProfileSection
+              title='francisco casiano'
+              subtitle='about'
+            />
+            {/* TODO: Find a way to style the scrollbar so it doesn't look so jarring? */}
+            <div className='overflow-y-auto flex-grow'>
+              {menuSectionPayloadExample.sections.map((section: any) => {
+                return (
+                  <MenuSections
+                    payload={section}
+                    key={section.title}
+                  />
+                );
+              })}
             </div>
-        </>
-    );
+            <div className='w-full my-2'>
+              <MenuFooter socialDetails={socialDetailsPayloadExample} />
+            </div>
+          </div>
+        </div>
+      </MenuBar>
+      <div className='h-screen flex flex-row bg-light-background dark:bg-dark-background'>
+        {/* Spacer to prevent page content get stuck behind the  */}
+        <div className='sm:w-1/3 lg:w-1/5 sm:max-w-xs'></div>
+        <div className='w-full h-full'>
+          poopy
+          {/* children */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MenuLayout;
