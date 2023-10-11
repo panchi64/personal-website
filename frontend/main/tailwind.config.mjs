693e3ce1a68ff80a7ad0b337c328f50d0ed4f39c
Francisco Casiano <personal@franciscocasiano.com>
2023-10-11 13:55:03 -0400

    renamed themes and added todo

--- frontend/main/tailwind.config.mjs
+++ frontend/main/tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const {nextui} = require("@nextui-org/react");
-export default {
+module.exports = {
     content: [
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
@@ -10,9 +10,10 @@ export default {
         extend: {},
     },
     darkMode: "class",
+    // TODO: Figure out how to include my colors into the nextui theme
     plugins: [nextui({
         prefix: "mac", // prefix for themes variables
-        addCommonColors: true, // override common colors (e.g. "blue", "green", "pink").
+        addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
         defaultTheme: "dark", // default theme from the themes object
         defaultExtendTheme: "dark", // default theme to extend on custom themes
         layout: {}, // common layout tokens (applied to all themes)
@@ -31,25 +32,33 @@ export default {
                     'item-border': `#e4e4e4`,
 
                     'callout-background': `#f5f5f7`,
-                    'callout-success': `#cff3cf`,
-                    'callout-success-border': `#75dc75`, //darkened success color by 25% (equivalent to darken(#cff3cf, 25%)
-                    'callout-warning': `#fefeee`,
-                    'callout-warning-border': `#f7f77a`, //darkened warning color by 25% (equivalent to darken(#fefeee, 25%)
+
                     'callout-error': `#f9d5d8`,
                     'callout-error-border': `#eb707a`, //darkened error color by 25% (equivalent to darken(#f9d5d8, 25%)
-
                     'red': `#ff3b30`,
                     'orange': `#ff9500`,
+
+                    'callout-warning': `#fefeee`,
+                    'callout-warning-border': `#f7f77a`, //darkened warning color by 25% (equivalent to darken(#fefeee, 25%)
                     'yellow': `#ffcc00`,
+
+
+                    'callout-success': `#cff3cf`,
+                    'callout-success-border': `#75dc75`, //darkened success color by 25% (equivalent to darken(#cff3cf, 25%)
                     'green': `#28cd41`,
+
                     'mint': `#00c7be`,
                     'teal': `#59adc4`,
                     'cyan': `#55bef0`,
                     'blue': `#007aff`,
+
                     'indigo': `#5856d6`,
                     'purple': `#af52de`,
+
                     'pink': `#ff2d55`,
+
                     'brown': `#a2845e`,
+
                     'gray': `#8e8e93`,
                     'black': `#1d1d1f`,
                 },
@@ -68,31 +77,38 @@ export default {
                     'item-border': `#343334`,
 
                     'callout-background': `#1d1d1f`,
+
                     'callout-success': `#022d02`,
                     'callout-success-border': `#44ea44`, //darkened success color by 25% (equivalent to darken(#cff3cf, 25%)
+                    'green': `#32d74b`,
+
                     'callout-warning': `#2d2c02`,
                     'callout-warning-border': `#eae544`, //darkened warning color by 25% (equivalent to darken(#fefeee, 25%)
+                    'yellow': `#ffd60a`,
+
                     'callout-error': `#330000`,
                     'callout-error-border': `#ff3037`, //darkened error color by 25% (equivalent to darken(#f9d5d8, 25%)
-
                     'red': `#ff453a`,
+
                     'orange': `#ff9f0a`,
-                    'yellow': `#ffd60a`,
-                    'green': `#32d74b`,
+
                     'mint': `#66d4cf`,
                     'teal': `#6ac4dc`,
                     'cyan': `#5ac8f5`,
                     'blue': `#0a84ff`,
+
                     'indigo': `#5e5ce6`,
                     'purple': `#bf5af2`,
+
                     'pink': `#ff375f`,
+
                     'brown': `#ac8e68`,
+
                     'gray': `#98989d`,
                     'white': `#ffffff`,
                     'black': `#000000`,
                 },
             },
-            // ... custom themes
         },
     })],
 }
